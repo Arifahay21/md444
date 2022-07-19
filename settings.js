5da3a4cc690a2efaa6b04f81a0815a1968716005
fachri <108580447+Fachri31@users.noreply.github.com>
2022-07-19 17:28:34 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -100,7 +100,7 @@ global.autorep = []
 global.ntilink = []
 
 //omther
-global.sessionName = 'session'
+global.sessionName = 'session.json'
 global.antitags = true
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
