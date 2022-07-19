60553527cfc9c147c9d3a8565652ddb190558a14
fachri <108580447+Fachri31@users.noreply.github.com>
2022-07-19 17:22:40 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['6288227758096'] //ur owner number
+global.ownername = "🦄Fachri" //ur owner name
+global.ytname = "YT: Fachri??" //ur yt chanel name
+global.socialm = "GitHub: Fachri??" //ur github or insta name
+global.location = "Indonesia,Jawa Tengah,Boyolali" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
+global.owner = ['6288227758096']
+global.ownertag = '6288227758096' //ur tag number
+global.botname = 'FazhBoT' //ur bot name
+global.linkz = "Chat Owner" //your theme url which will be displayed on whatsapp
+global.websitex = "Chat Owner" //ur website to be displayed
+global.botscript = 'https://github.com/Fachri31/CheemsBot-MD4' //script link
+global.reactmoji = "🗿" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.packname = "Fazh Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
+global.author = "🦄Fachri" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['6288227758096'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
@@ -106,14 +106,14 @@ global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
     success: 'Done ✓',
-    admin: 'This Feature Is Only For Admin!',
-    botAdmin: 'Bot Must Be Admin First!',
-    owner: 'This Feature Is Only For Owner!',
-    group: 'Feature Used Only For Groups!',
-    private: 'Features Used Only For Private Chat!',
-    bot: 'This Feature Is Only For Bot',
-    wait: 'In process...',
-    linkm: 'Where is the link?',
+    admin: 'Fitur Ini Hanya Untuk Admin!',
+    botAdmin: 'Bot Harus Admin Dulu!',
+    owner: 'Fitur Ini Hanya Untuk Owner!',
+    group: 'Fitur Ini Hanya Untuk Group!',
+    private: 'Fitur Ini Hanya Untuk Private Chat!',
+    bot: 'Fitur Ini Hanya Untuk Bot',
+    wait: '[Loading....]',
+    linkm: 'Mana Link nya?',
     error: 'Error!!',
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
     ban: 'You have been banned by the owner, if you want to be unbanned, chat owner.',
